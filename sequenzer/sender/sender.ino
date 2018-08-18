d9da5403736d8f6629be7498aad06d7888df0de0
Stanley Klemme <st@nley-klemme.de>
2018-08-18 15:24:53 +1000

    housekeeping

--- sequenzer/sender/sender.ino
+++ sequenzer/sender/sender.ino
@@ -19,50 +19,20 @@ int i;
 
 // Set up nRF24L01 radio on SPI bus plus pins 8 & 9
 RF24 radio( 9, 10 );
-
-
-//
 // Topology
 //
-
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
-//
-// Role management
-//
-// Set up role.  This sketch uses the same software for all the nodes
-// in this system.  Doing so greatly simplifies testing.  
-//
-
-// The various roles supported by this sketch
-typedef enum { role_ping_out = 1, role_pong_back } role_e;
-
-// The debug-friendly names of those roles
-const char* role_friendly_name[] = { "invalid", "Ping out", "Pong back"};
-
-// The role of the current running sketch
-role_e role = role_ping_out;
-
-//old stuff and modifiers
 // array to hold read audio levels
 int m_freqVal[7];
-// potentiometer output value [0,1023]
-unsigned int m_potValue = 0U;
 // debug flags
 const boolean m_potDebug = true;
 const boolean m_sequencerDebug = true;
-// serial boud rate: 9600 seems to work best
-//const unsigned int c_boudRate = 9600U;
+// serial boud rate: 57600 seems to work best
 const unsigned int c_boudRate = 57600U;
 // frequenzy bands used (<=7)
 const unsigned int c_frequencyBands = 7U;
-// potentiometer analog read channel
-const unsigned int c_potentionMeterInput = 3U;
-// noise gate value for frequency levels
-const unsigned int c_noiseGate = 100U;
-// divider for potentiometer read value
-const unsigned int c_potDevider = 1U;
 
 void setup()
 {
@@ -89,104 +59,70 @@ void setup()
   Serial.begin( c_boudRate );
 
   printf_begin();
-  printf("\n\rRF24/examples/GettingStarted/\n\r");
-  printf("ROLE: %s\n\r",role_friendly_name[role]);
-  printf("*** PRESS 'T' to begin transmitting to the other node\n\r");
-
-  //
   // Setup and configure rf radio
-  //
-
   radio.begin();
-
+  radio.setDataRate(RF24_250KBPS);
+  //radio.enableDynamicPayloads();
   // optionally, increase the delay between retries & # of retries
   radio.setRetries(20,15);
 
-  // optionally, reduce the payload size.  seems to
-  // improve reliability
-  //radio.setPayloadSize(8);
-
-  //
-  // Open pipes to other nodes for communication
-  //
-
-  // This simple sketch opens two pipes for these two nodes to communicate
-  // back and forth.
-  // Open 'our' pipe for writing
-  // Open the 'other' pipe for reading, in position #1 (we can have up to 5 pipes open for reading)
-
-  if( role == role_ping_out )
-  {
-    radio.openWritingPipe(pipes[0]);
-    radio.openReadingPipe(1,pipes[1]);
-  }
-  else
-  {
-    radio.openWritingPipe(pipes[1]);
-    radio.openReadingPipe(1,pipes[0]);
-  }
-
-  //
+  radio.openWritingPipe( pipes[0] );
+  radio.openReadingPipe( 1, pipes[1] );
   // Start listening
-  //
-
-    // First, stop listening so we can talk.
-    radio.startListening();
-
-  //
+  radio.startListening();
   // Dump the configuration of the rf unit for debugging
-  //
-
   radio.printDetails();
 }
 
 void loop()
 {
-  ReadFrequencies();
-
-  delay(100);
-  
-  //return 7 values of 7 bands pass filter
+    delay(100);  
+    //return 7 values of 7 bands pass filter
+    ReadFrequencies();
     //Frequency(Hz):63  160  400  1K  2.5K  6.25K  16K
     //FreqVal[]:    0    1    2    3    4    5    6
-//    //devide pot value for coarser resolution / easier adjustment
-    //m_potValue = max( ( analogRead( c_potentionMeterInput ) / c_potDevider ), 0U );
-
-    //const unsigned int freq = ( m_freqVal[ x ] > c_noiseGate ) ? ( m_freqVal[ x ] - c_noiseGate ) : 0U;
     if( m_sequencerDebug )
     {
         DebugPrintFrequencies();
     }
+    SendValues();
+}
 
+void SendValues()
+{
     radio.stopListening();
     // Take the time, and send it.  This will block until complete
-    bool ok = radio.write( &m_freqVal, sizeof(m_freqVal) );
-    
-    if (ok)
-      printf("=======================  ok...\n\r");
-    else
-      printf("failed=========================.\n\r");
+    bool ok = radio.write( m_freqVal, sizeof(m_freqVal) );
 
+    if( ok )
+    {
+      printf("=======================  ok...\n\r");
+    }
+    else
+    {
+      printf("failed=========================.\n\r");
+    }
     radio.startListening();
-    
-    
-    //debug output for potentiometer
-    //if( m_potDebug )
-    //{
-    //    Serial.print( m_potValue );
-    //    Serial.println();
-    //}
 }
 
 void ReadFrequencies()
 {
   //Read frequencies for each band
-  for( freq_amp = 0; freq_amp < 7; freq_amp++ )
+  for( freq_amp = 0; freq_amp < c_frequencyBands; freq_amp++ )
   {
     Frequencies_One[ freq_amp ] = analogRead( DC_One );
     Frequencies_Two[ freq_amp ] = analogRead( DC_Two ); 
     digitalWrite(STROBE, HIGH);
     digitalWrite(STROBE, LOW);
+  }
+  //for now take the higher channel
+  if( Frequencies_Two[ i ] > Frequencies_One[ i ] )
+  {
+      m_freqVal[ i ] = Frequencies_Two[ i ];
+  }
+  else
+  {
+      m_freqVal[ i ] = Frequencies_One[ i ];
   }
 }
 
@@ -194,14 +130,6 @@ void DebugPrintFrequencies()
 {
     for( i = 0; i < 7; i++ )
     {
-        if( Frequencies_Two[ i ] > Frequencies_One[ i ] )
-        {
-            m_freqVal[ i ] = Frequencies_Two[ i ];
-        }
-        else
-        {
-            m_freqVal[ i ] = Frequencies_One[ i ];
-        }
         Serial.print( m_freqVal[ i ] );
         Serial.print( ' ' );
    }
