cd8fda80563288256b08af38528c0ca20aa0dc6d
Stanley Klemme <st@nley-klemme.de>
2018-09-07 16:32:31 +1000

    print values to LCD

--- sequenzer/sender/sender.ino
+++ sequenzer/sender/sender.ino
@@ -1,3 +1,10 @@
+#include <Wire.h>
+
+//LCD display
+#include <LiquidCrystal_I2C.h>
+#include <I2CIO.h>
+#include <LCD.h>
+
 // wifi uses SPI
 #include <SPI.h>
 #include <RF24.h>
@@ -5,6 +12,7 @@
 #include <nRF24L01.h>
 //custom printf
 #include "printf.h"
+
 //Declare Spectrum Shield pin connections
 #define STROBE 4
 #define RESET 6
@@ -23,6 +31,9 @@ RF24 radio( 9, 10 );
 //
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+//LCD Display
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
 
 // array to hold read audio levels
 int m_freqVal[7];
@@ -86,6 +97,11 @@ void setup()
   radio.startListening();
   // Dump the configuration of the rf unit for debugging
   radio.printDetails();
+
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH); // NOTE: You can turn the backlight off by setting it to LOW instead of HIGH
+  lcd.begin(16, 2);
+  lcd.clear();
 }
 
 void loop()
@@ -100,8 +116,30 @@ void loop()
         DebugPrintFrequencies();
     }
     ArrayToFreqs();
+    PrintFreqOnLCD();
   //  DummyToFreqs();
     SendValues();
+}
+
+
+void PrintFreqOnLCD()
+{
+  lcd.setCursor(0,0);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq0);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq1);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq2);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq3);
+  lcd.setCursor(0,1);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq4);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq5);
+  lcd.print(' ');
+  lcd.print(m_freqs.freq6);
 }
 
 void ArrayToFreqs()
