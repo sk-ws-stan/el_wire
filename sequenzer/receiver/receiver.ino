f37ffb5a70a065d162063b448ee8581ecaad4d1c
Stanley Klemme <st@nley-klemme.de>
2018-09-15 15:55:36 +1000

    multicast receiver and clean up

--- sequenzer/receiver/receiver.ino
+++ sequenzer/receiver/receiver.ino
@@ -6,14 +6,14 @@
 
 #include <EL_Escudo.h>
 
-RF24 radio( 9, 10 );
+RF24 m_radio( 9, 10 );
 
 //
 // Topology
 //
 
 // Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+const uint64_t c_pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
 // array to hold read audio levels
 int m_freqVal[7];
@@ -23,8 +23,6 @@ unsigned int m_potValue = 0U;
 const boolean c_potDebug = false;
 const boolean c_radioDebug = true;
 const boolean c_sequencerDebug = false;
-// serial boud rate: 9600 seems to work best
-//const unsigned int c_boudRate = 9600U;
 const unsigned int c_boudRate = 57600U;
 // frequenzy bands used (<=7)
 const unsigned int c_frequencyBands = 7U;
@@ -54,18 +52,17 @@ void setup()
   Serial.begin( c_boudRate );
 
   printf_begin();
-  radio.begin();
-  radio.setRetries(15,15);
-  //radio.enableDynamicPayloads();
-  radio.setAutoAck( false );
-  //radio.setPayloadSize(8);
+  m_radio.begin();
+  m_radio.enableDynamicPayloads();
+  m_radio.enableDynamicAck();
+  m_radio.setAutoAck( false );
   // Open reading pipes
-  radio.openWritingPipe( pipes[1] );
-  radio.openReadingPipe( 1, pipes[0] );
+  m_radio.openWritingPipe( c_pipes[1] );
+  m_radio.openReadingPipe( 1, c_pipes[0] );
   // Start listening
-  radio.startListening();
+  m_radio.startListening();
   // Dump the configuration of the rf unit for debugging
-  radio.printDetails();
+  m_radio.printDetails();
 
   // init EL_Escudo's EL channels: digital pins 2 through 9
   pinMode( 2, OUTPUT );  // channel A
@@ -75,7 +72,7 @@ void setup()
   pinMode( 6, OUTPUT );  // channel E
   pinMode( 7, OUTPUT );  // channel F
   pinMode( 8, OUTPUT );  // channel G
-//  pinMode( 9, OUTPUT );  // channel H
+  //pinMode( 9, OUTPUT );  // channel H - taken by radio
   // init status LEDs: pin 10 on EL_Escudo and pin 13 on Arduino
   //pinMode( 10, OUTPUT );
   //pinMode( 13, OUTPUT );
@@ -127,14 +124,14 @@ void loop()
     //return 7 values of 7 bands pass filter
     //Frequency(Hz):63  160  400  1K  2.5K  6.25K  16K
     //FreqVal[]:    0    1    2    3    4    5    6
-    delay(20);
+    delay(10);
     //read wifi here
     ReadRadio();
     FreqsToArray();
     //devide pot value for coarser resolution / easier adjustment
     //disable this for now as it isn't connected
     //m_potValue = max( ( analogRead( c_potentionMeterInput ) / c_potDevider ), 0U );
-    m_potValue = 250U;    
+    m_potValue = 250U;
     //debug output for potentiometer
     if( c_potDebug )
     {
@@ -146,40 +143,32 @@ void loop()
 
 void ReadRadio()
 {
-    if( radio.available() )
+    uint8_t pipe_num;
+
+    if( m_radio.available( &pipe_num ) && pipe_num == 1 )
     {
-      //int test = -3;
-      // Dump the payloads until we've gotten everything
-      bool done = false;
-      while( !done )
+      uint8_t len = m_radio.getDynamicPayloadSize();
+
+      if( len == sizeof( m_freqs ) )
       {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read( &m_freqs, sizeof(m_freqs) );
-        //done = radio.read( &test, sizeof(test) );
+        Serial.print( "matches" );Serial.println();
       }
-      radio.stopListening();
+      m_radio.read( &m_freqs, sizeof( m_freqs ) );
+
+      //m_radio.stopListening();
 
       if( c_radioDebug )
       {
-        Serial.print( "==========" );
-        if( done )
-        {
-          Serial.print( "ok" );
-        }
-        else
-        {
-          Serial.print( "meeeehh" );
-        }
-        Serial.print( "==========" );
-        Serial.println();
+        Serial.print( m_freqs.freq0 ); Serial.print( " " );
+        Serial.print( m_freqs.freq1 ); Serial.print( " " );
+        Serial.print( m_freqs.freq2 ); Serial.print( " " );
+        Serial.print( m_freqs.freq3 ); Serial.print( " " );
+        Serial.print( m_freqs.freq4 ); Serial.print( " " );
+        Serial.print( m_freqs.freq5 ); Serial.print( " " );
+        Serial.print( m_freqs.freq6 ); Serial.print( " " );
 
-        for( unsigned int x = 0U; x < c_frequencyBands; x++ )
-        {
-            Serial.print( m_freqVal[ x ] ); Serial.print( " " );
-        }
-        //Serial.print( test );
         Serial.println();
       }
+      //m_radio.startListening();
     }
-    radio.startListening();
 }
